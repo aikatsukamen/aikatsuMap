715c2b7129595ba3b8872ecaa5322fd4ee88b8db
aikatsukamen <aikatsukamen@gmail.com>
2019-04-29 01:25:21 +0900

    initial

---
+++ src/js/index.ts
@@ -0,0 +1,32 @@
+import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import '../css/map.css';
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+const map = L.map('map').setView([43.067, 141.35], 16);
+
+// delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+});
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+  maxZoom: 18,
+}).addTo(map);
+
+// delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+});
+
+L.marker([43.067, 141.35])
+  .addTo(map)
+  .bindPopup('札幌駅前だよー')
+  .openPopup();
