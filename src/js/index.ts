a496ee79f20ae0e566423118b7c747242c27369b
aikatsukamen <aikatsukamen@gmail.com>
2019-05-24 23:54:08 +0900

    レイヤー変えられるようにした

--- src/js/index.ts
+++ src/js/index.ts
@@ -22,10 +22,35 @@ const zoom = 8;
 const map = L.map('map').setView([35.699024, 139.771062], zoom);
 
 // 地図情報を取ってくるところ
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-  maxZoom: 18,
-}).addTo(map);
+// オープンストリートマップ
+// L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+//   maxZoom: 18,
+// }).addTo(map);
+
+L.control.scale({ maxWidth: 200, position: 'bottomright', imperial: false }).addTo(map);
+L.control.zoom({ position: 'bottomleft' }).addTo(map);
+
+// 地理院地図の標準地図タイル
+const gsi = L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+  attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>",
+});
+// 地理院地図の淡色地図タイル
+const gsipale = L.tileLayer('http://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
+  attribution: "<a href='http://portal.cyberjapan.jp/help/termsofuse.html' target='_blank'>地理院タイル</a>",
+});
+// オープンストリートマップのタイル
+const osm = L.tileLayer('http://tile.openstreetmap.jp/{z}/{x}/{y}.png', { attribution: "<a href='http://osm.org/copyright' target='_blank'>OpenStreetMap</a> contributors" });
+
+const baseMaps = {
+  地理院地図: gsi,
+  淡色地図: gsipale,
+  オープンストリートマップ: osm,
+};
+// layersコントロールにbaseMapsオブジェクトを設定して地図に追加
+// コントロール内にプロパティ名が表示される
+L.control.layers(baseMaps).addTo(map);
+gsi.addTo(map);
 
 /** ヒートマップの座標リスト */
 const heatPoints: [number, number, number][] = [];
@@ -46,3 +71,5 @@ for (const store of storeData) {
 const heatOption: L.HeatMapOptions = { radius: 25 };
 // ヒートマップ描画
 L.heatLayer(heatPoints, heatOption).addTo(map);
+
+// ベースレイヤ切り替え
